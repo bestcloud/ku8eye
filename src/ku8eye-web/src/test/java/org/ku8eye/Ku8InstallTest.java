18b0720d063e303a63a1ff50bed0ca95d78ab8a4
guiyunuo <guiyunuo@qq.com>
2015-12-02 11:16:32 +0800

    安装测试类

---
+++ src/ku8eye-web/src/test/java/org/ku8eye/Ku8InstallTest.java
@@ -0,0 +1,30 @@
+package org.ku8eye;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ku8eye.service.deploy.Ku8ClusterDeployService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = { App.class })
+public class Ku8InstallTest {
+	
+
+	private org.ku8eye.service.deploy.ProcessCaller ProcessCaller = new org.ku8eye.service.deploy.ProcessCaller();
+
+	@Test
+	public void getTemp() {
+//		deployService.getAllTemplates();
+	}
+
+	@Test
+	public void getRunCommand() throws Exception {
+
+		ProcessCaller.call("notepad.exe");
+		for (String s : ProcessCaller.getOutputs()) {
+			System.out.println(s);
+		}
+	}
+}
