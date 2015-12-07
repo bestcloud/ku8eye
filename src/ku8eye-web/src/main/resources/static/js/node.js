1617f38c15ae20a1625f1605b3e47f9e8ba50f4d
zhangying3 <ying.zhang@jadefortune.com.cn>
2015-12-07 18:54:59 +0800

    安装环境页面

--- src/ku8eye-web/src/main/resources/static/js/node.js
+++ src/ku8eye-web/src/main/resources/static/js/node.js
@@ -98,10 +98,10 @@
 					case "cluster_main":
 						pageIndex( settings.loading, settings.first );
 						break;
-					case "mode":
+					case "singleNode":
 						pageRate();
 						break;
-					case "indtallProgress":
+					case "multiNode":
 						pageDetail();
 						break;
                     case "testResult":
@@ -141,8 +141,8 @@
 		            $.each(d.cluster,function(i,item){
 		            	tableList += [
 		                     count%2==1?'<tr>':'',
-		                     count%2==1?'<th id="'+item.id+'"><ul class="Mode"><li><h3>'+item.name+'</h3></li><li class="mode_li">要求：最少'+item.minNodes+'节点,最大'+item.maxNodes+'节点</li><li class="mode_li">'+item.describe+'</li></ul></th>':'',
-		                     count%2==0?'<th id="'+item.id+'"><ul><li><h3>'+item.name+'</h3></li><li class="mode_li">要求：最少'+item.minNodes+'节点,最大'+item.maxNodes+'节点</li><li class="mode_li">'+item.describe+'</li></ul></th>':'',
+		                     count%2==1?'<th id="'+item.id+'" c="'+item.maxNodes+'"><ul class="Mode"><li><h3>'+item.name+'</h3></li><li class="mode_li">要求：最少'+item.minNodes+'节点,最大'+item.maxNodes+'节点</li><li class="mode_li">'+item.describe+'</li></ul></th>':'',
+		                     count%2==0?'<th id="'+item.id+'" c="'+item.maxNodes+'"><ul><li><h3>'+item.name+'</h3></li><li class="mode_li">要求：最少'+item.minNodes+'节点,最大'+item.maxNodes+'节点</li><li class="mode_li">'+item.describe+'</li></ul></th>':'',
 		                     count%2==0?'</tr>':''
 		                ].join('');
 		               
@@ -153,14 +153,20 @@
 		            
 		            
 		            $( '#example1' ).find('th').bind('click',function(){
-//		            	par.id=$( this ).attr('id');
-		            	$( '.content' ).pageControl({
-							page: "mode"
-						});
-
-						$( '.content' ).data({
+		            	$( '.content' ).data({
 							'itemId': $( this ).attr('id')
 						});
+		            	
+		            	if($( this ).attr('c')==1){
+		            		$( '.content' ).pageControl({
+								page: "singleNode"
+							});
+		            	}else{
+		            		$( '.content' ).pageControl({
+								page: "multiNode"
+							});
+		            	}
+		            	
 					});
 		            
 	            }
@@ -169,30 +175,16 @@
             
 		}; 
 		
-		
+		//单节点的js
 		function pageRate(){
 			var itemId = $( '.content' ).data( 'itemId' );
-			$("#mode_h5").val();
-			$.ajax({
-		        url:"/deploycluster/selecttemplate/"+itemId,
-		        type: "GET",
-		        dataType:"json",
-		        success: function(data){
-		        	d.node=data;
-		        	$("#mode_h5").html("您选择了"+data.name+"模式，至少需要"+data.minNodes+"节点，建议"+data.maxNodes+"个节点");
-		        	$.each(data.nodes,function(i,item){
-		        		$("#example").append("<tr><td><input type='checkbox' value='"+item.hostId+"' class='chkbox' name='child' /></td><td>"+item.ip+"</td><td>"+item.hostName+"</td><td>"+item.hostName+"</td></tr>");
-		        	});
-		        }
-		    }); 
-			
-			//添加节点对话框的值
+			//要添加的节点对话框的值
 			$('#example1').DataTable({
 		        "ajax":"/addlist/1",
 		        "columns": [
 		        			{ "data": "id", render: function ( data, type, row ) {
 		                		// Combine the first and last names into a single table field
-		                			return "<input type='checkbox' value='"+data+"' class='chkbox' name='child' />";
+		                			return "<input type='radio' value='"+data+"' class='chkbox' name='child' checked='checked'/>";
 		            		} },
 		        			{ "data": "hostName" },
 		        			{ "data": "ip" },
@@ -212,37 +204,185 @@
 		    });
 			
 			
+			//模版默认的值显示
+			$.ajax({
+		        url:"/deploycluster/selecttemplate/"+itemId,
+		        type: "GET",
+		        dataType:"json",
+		        success: function(data){
+//		        	d.node=data;
+		        	$("#mode_h5").html("您选择了"+data.name+"模式，至少需要"+data.minNodes+"节点，建议"+data.maxNodes+"个节点");
+
+		        	$("#nodeIp").html(data.nodes[0].ip);
+		        	$("#nodeName").html(data.nodes[0].hostName);
+		        	
+		        	$.each(data.nodes[0].nodeRoleParams,function(i,item){
+		        		$.each(item,function(m,n){
+		        			$("#example").append("<tr><td>"+i+"</td><td>"+n.name+"</td><td><input type='text' value='"+n.value+"'/></td><td>"+n.describe+"</td></tr>");
+		        		});
+		        	});
+		        	
+		        }
+		    }); 
+			
+			//按保存，把数据添加到页面
 			$("#save").click(function(){
-		    	
-		    	var de="";
-		    	$(":checkbox:checked").closest("tr").find(":checkbox").each(function(i, eleDom) {
+				var td = $(".chkbox:checked").first().parent("td");
+				td = $(td).next("td");
+				var nodename = jQuery.trim($(td).text());
+				td = $(td).next("td");
+				var nodeip = jQuery.trim($(td).text());
+				$("#nodeIp").html(nodeip);
+	        	$("#nodeName").html(nodename);
+			
+		    });
+			
+			
+			
+			$("#nextStep").click(function(){
+				alert("下一步");
+				var de="";
+				$(":checkbox:checked").closest("#example tr").find(":checkbox").each(function(i, eleDom) {
 					// 遍历每个被选中的复选框所在行的文本框的值
 					de += eleDom.value + ",";
 				});
-		    	
-		    	
-		    	$.ajax({
-			        url:"/hostlist/"+de,
-			        type: "GET",
-			        dataType:"json",
-			        success: function(data){
-			        	alert(data);
-			        	var node1="";
-			        	var node2="";
-			        	$.each(d.node.nodes,function(i,item){
-			        		node1+="<tr><td><input type='checkbox' value='"+item.hostId+"' class='chkbox' name='child' /></td><td>"+item.ip+"</td><td>"+item.hostName+"</td><td>"+item.hostName+"</td></tr>";
-			        	});
-			        	$.each(data,function(i,item){
-			        		node2+="<tr><td><input type='checkbox' value='"+item.hostId+"' class='chkbox' name='child' /></td><td>"+item.ip+"</td><td>"+item.hostName+"</td><td>"+item.hostName+"</td></tr>";
-			        	});
-			        	$("#example").html(node1+node2);
-			        }
-			    }); 
-			
 				
-		    });
+				$( '.content' ).data({
+					'zoneId': de
+				});
+				
+				$( '.content' ).pageControl({
+					page: "modeNext"
+				});
+				
+			});
+			
+//			$("#exect").click(function(){
+//				if ($("#exect").attr("checked")) { 
+//			        $(":checkbox").attr("checked", false);  
+//			       
+//			    } else {
+//			        $(":checkbox").attr("checked", true);  
+//			    }   
+//			});
+			
+						
+			
 
 		}
+		
+		
+		//多节点的js
+		function pageDetail(){
+			var itemId = $( '.content' ).data( 'itemId' );
+			var li="";
+			var node="";
+			var arr=[];
+			var count=0;
+			$('#example1').DataTable({
+		        "ajax":"/addlist/1",
+		        "columns": [
+		        			{ "data": "id", render: function ( data, type, row ) {
+		                		// Combine the first and last names into a single table field
+		                			return "<input type='checkbox' value='"+data+"' class='chkbox' name='child'/>";
+		            		} },
+		        			{ "data": "hostName" },
+		        			{ "data": "ip" },
+		        			{ "data": "lastUpdated" },
+		        			{ "data": "cores" },
+		        			{ "data": "location" },
+		        			
+			       		],
+		       		"buttons": [
+		       		          {
+		       		              text: 'Reload',
+		       		              action: function ( e, dt, node, config ) {
+		       		                  dt.ajax.reload();
+		       		              }
+		       		          }
+		       		      ]
+		    });
+			
+			//模版默认的值显示
+			$.ajax({
+		        url:"/deploycluster/selecttemplate/"+itemId,
+		        type: "GET",
+		        dataType:"json",
+		        success: function(data){
+//		        	d.node=data;
+		        	$("#mode_h5").html("您选择了"+data.name+"模式，至少需要"+data.minNodes+"节点，建议"+data.maxNodes+"个节点");
+		        	$.each(data.nodes,function(i,item){
+		        		arr[count]=item.hostName;
+		        		count++;
+		        	});
+		        
+	        		var str = []; 
+	        		for(var i = 0;i < arr.length;i++){ 
+	        		! RegExp(arr[i],"g").test(str.join(",")) && (str.push(arr[i])); 
+	        		} 
+	        		for(var j=0;j<str.length;j++){
+	        			li += [
+	    				       '<div style="padding: 10px 0px 7px 0px;">',
+	    				       '<span>kubernetes:</span>',
+	    				       '<span>'+str[j]+'</span>',
+	    				       '</div>',
+
+	    				       '<table id="'+str[j]+'" class="table table-bordered table-hover" style="font-size: 12px;">',
+	    				       '<thead>',
+	    				       '<tr>',
+	    				       '<th>角色</th>',
+	    				       '<th>参数名</th>',
+	    				       '<th>参数值</th>',
+	    				       '<th>描述</th>',
+	    				       '</tr>',
+	    	
+	    				       '</thead>',
+	    				       '</table >',
+	    				       '<div style="padding: 15px 0px 7px 0px;">',
+	    				       '<span >kubernetes:Nodes</span>',
+	    				       '<a  class="btn btn-primary " data-toggle="modal" data-target="#myModal2">添加节点</a>',	
+	    				       '</div>',
+	    				       '<table id="'+str[j]+'0" class="table table-bordered table-hover" style="font-size: 12px;">',
+	    				       '<thead>',
+	    				       '<tr>',
+	    				       '<th>节点</th>',
+	    				       '<th>节点参数</th>',
+	    				       '<th>操作</th>',
+	    				       '</tr>',
+	    				       '</thead>',
+	    				       '</table>',
+	    				       '<div style="margin-top:20px;border-top:1px solid;width:100%"></div>'   
+	                       ].join('');
+	        		}
+	        		
+	        		
+	        		$("#multiNodes").html(li);
+	        		$.each(data.nodes,function(i,item){
+	        			
+	        			$('table[id="'+item.hostName+'0"]').append("<tr><td>"+item.ip+"</td><td>"+item.hostName+"</td><td><span>设置</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>删除</span></td></tr>");
+	        			
+	        			$.each(item.nodeRoleParams,function(m,n){
+	        				$.each(n,function(a,b){
+		        				$('table[id="'+item.hostName+'"]').append("<tr><td>"+item.hostName+"</td><td>"+b.name+"</td><td>"+b.value+"</td><td>"+b.describe+"</td></tr>");
+		        				 
+	        				});
+	        				
+	        			});
+		        	});
+
+		        }
+		    }); 
+			
+			//按保存，把数据添加到页面
+			$("#save").click(function(){
+				
+			
+		    });
+		
+		}
+//		function switchSysBar(){
+//			alert("sdfsdfsdf");
+//		}
 		
 	};
 
