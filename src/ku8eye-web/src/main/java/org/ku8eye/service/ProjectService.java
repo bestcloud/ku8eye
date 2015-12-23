4660b533d96156c978cc49c2c629c5e985d076b4
fecogc <feco_gc@hotmail.com>
2015-12-23 16:54:07 +0800

    -Fixed add Application Controller -HTML, CSS, Fonts Added -Linked Applications with SQL

--- src/ku8eye-web/src/main/java/org/ku8eye/service/ProjectService.java
+++ src/ku8eye-web/src/main/java/org/ku8eye/service/ProjectService.java
@@ -1,5 +1,6 @@
 package org.ku8eye.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.ku8eye.domain.Ku8Project;
@@ -28,5 +29,19 @@ public class ProjectService {
 	@Transactional(propagation = Propagation.NOT_SUPPORTED)
 	public List<Ku8Project> getAllProjects() {
 		return projectDao.selectAll();
+	}
+	
+	@Transactional(propagation = Propagation.NOT_SUPPORTED)
+	public int addProject(int tenantId, String owner, String name, String version, String k8sVersion, String note) {
+		Ku8Project ku8p = new Ku8Project();
+		ku8p.setTenantId(tenantId);
+		ku8p.setOwner(owner);
+		ku8p.setName(name);
+		ku8p.setVersion(version);
+		ku8p.setK8sVersion(k8sVersion);
+		ku8p.setNote(note);
+		ku8p.setLastUpdated(new Date());
+		
+		return projectDao.insert(ku8p);
 	}
 }
