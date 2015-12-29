a5472f89dde5bed0624ded87304cef1782205390
zhangying3 <ying.zhang@jadefortune.com.cn>
2015-12-29 17:38:18 +0800

    镜像详情

--- src/ku8eye-web/src/main/java/org/ku8eye/service/UIService.java
+++ src/ku8eye-web/src/main/java/org/ku8eye/service/UIService.java
@@ -61,7 +61,10 @@ public class UIService {
 		Menu firstMenu4 = new Menu("projects", "My Applications", "", MENU_TYPE_PROJECT_GROUP);
 		menus.add(firstMenu4);
 
-		Menu childMenu = new Menu("project-list", "List ", "project_main.html", MENU_TYPE_PROJECT_NODE);
+		Menu childMenu = new Menu("project-docker", "docker ", "project_docker.html", MENU_TYPE_PROJECT_NODE);
+		firstMenu4.getSubMenus().add(childMenu);
+		
+		childMenu = new Menu("project-list", "List ", "project_main.html", MENU_TYPE_PROJECT_NODE);
 		firstMenu4.getSubMenus().add(childMenu);
 
 		childMenu = new Menu("project-report", "Report ", "project_report.html", MENU_TYPE_PROJECT_NODE);
