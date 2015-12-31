e6a23053768716bc0cce05ab599f3a36262abb09
zhihui.wu@hp.com <mycat1314>
2015-12-31 16:17:55 +0800

    更新微服务等的域对象定义

--- src/ku8eye-web/src/main/java/org/ku8eye/service/ProjectService.java
+++ src/ku8eye-web/src/main/java/org/ku8eye/service/ProjectService.java
@@ -55,7 +55,7 @@ public class ProjectService {
 		ku8p.setVersion(version);
 		ku8p.setK8sVersion(k8sVersion);
 		ku8p.setNote(note);
-		ku8p.setYamlSpec(yamlSpec);
+		ku8p.setJsonSpec(yamlSpec);
 		ku8p.setLastUpdated(new Date());
 		
 		return projectDao.updateByPrimaryKey(ku8p);
