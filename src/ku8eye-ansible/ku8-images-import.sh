4f6cc028b2ccd5c0653144659b84ed92950a234c
yaoyu26 <yu.yao@jadefortune.com.cn>
2016-01-06 16:20:22 +0800

    导入脚本加参数

--- src/ku8eye-ansible/ku8-images-import.sh
+++ src/ku8eye-ansible/ku8-images-import.sh
@@ -1,6 +1,7 @@
 FILE=$1
+CLUSTERID=$2
 gunzip FILE
 FILE=${FILE:0:${#FILE}-3} 
 cd $FILE
 PATH=pwd
-java -jar /root/ku8eye-web.jar org.ku8eye.App image -p $PATH
+java -jar /root/ku8eye-web.jar org.ku8eye.App image -p $PATH -c $CLUSTERID 
